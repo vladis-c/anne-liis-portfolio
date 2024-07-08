af27d62320b2f44cd4a15cfa348ccb1b5b47a8b0
Vladislav Cherkasheninov <vladislav.cherkasheninov@taiste.com>
2024-07-08 11:09:32 +0300

    style for Hero block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import NextImage from 'next/image';
 import BgImage from '@/components/BgImage';
 import {getHomeContent} from '@/api/getHomeContent';
-import Contentful, {H5} from '@/components/Contentful/Contentful';
+import Contentful, {H5, P} from '@/components/Contentful/Contentful';
 import ArrowRight from '@/svgs/ArrowRight';
 import VerticalDivider from '@/svgs/VerticalDivider';
 import Instagram from '@/svgs/Instagram';
@@ -14,8 +14,10 @@ export default async function Home() {
   }
   const {
     navigation: {bgImage, name, menu, contacts},
-    hero: {image},
+    hero: {image, heroText, heroTitle},
   } = homeContent;
+
+  console.log(heroText);
 
   return (
     <main className="flex flex-col items-center justify-between">
@@ -24,7 +26,7 @@ export default async function Home() {
       {/* Navigation content */}
       <div className="h-360 sm:h-360 md:h-480 lg:h-720 xl:h-1080 flex flex-row items-start justify-center sm:justify-center md:justify-between lg:justify-between xl:justify-between w-full p-6 sm:p-12 md:p-12 lg:p-12 xl:p-12">
         {/* Menu */}
-        <div className="hidden sm:hidden md:flex lg:flex xl:flex flex-row justify-start items-center">
+        <div className="hidden md:flex flex-row justify-start items-center">
           {menu.map((el, i, self) => {
             if (i === self.length - 1) {
               return <H5 key={el}>{el}</H5>;
@@ -43,12 +45,12 @@ export default async function Home() {
         {/* Logo (name) */}
         <Contentful
           document={name}
-          className="w-64 sm:w-64 md:w-32 lg:w-64 xl:w-64 text-center"
+          className="w-64 md:w-32 lg:w-64 text-center"
         />
         {/* Contacts */}
-        <div className="hidden sm:hidden md:flex lg:flex xl:flex flex-row justify-start items-center">
+        <div className="hidden md:flex flex-row justify-start items-center">
           {contacts.map((el, i, self) => {
-            const Element = () => {
+            const Contact = () => {
               if (el.toLowerCase() === 'instagram') {
                 return (
                   <div className="relative -top-1" key={el}>
@@ -61,11 +63,11 @@ export default async function Home() {
             };
 
             if (i === self.length - 1) {
-              return <Element key={el} />;
+              return <Contact key={el} />;
             } else {
               return (
                 <div className="flex flex-row" key={el}>
-                  <Element />
+                  <Contact />
                   <div className="ml-2 mr-2 relative -top-1">
                     <VerticalDivider />
                   </div>
@@ -76,18 +78,26 @@ export default async function Home() {
         </div>
       </div>
       {/* Hero content */}
-      <div className="w-full h-1080 bg-indigo-custom bg-transparent">
-        {/* Picture */}
-        <div className="overflow-hidden w-360 h-480 rounded-full">
-          <NextImage
-            priority
-            src={image}
-            alt={'Hero Image'}
-            width={360}
-            height={480}
-            placeholder="empty"
-            className="w-full h-full object-cover"
-          />
+      <div className="flex flex-col lg:flex-row justify-start lg:justify-between items-center w-full h-1080 bg-indigo-custom bg-transparent">
+        {/* Left block */}
+        <div className="flex justify-center items-center w-full lg:w-1/2 h-auto md:h-full px-12 lg:px-24 py-12 md:py-24">
+          {/* Picture */}
+          <div className="overflow-hidden w-360 h-480 rounded-full">
+            <NextImage
+              priority
+              src={image}
+              alt={'Hero Image'}
+              width={360}
+              height={480}
+              placeholder="empty"
+              className="w-full h-full object-cover"
+            />
+          </div>
+        </div>
+        {/* Right block */}
+        <div className="flex flex-col justify-center items-center lg:items-start w-full lg:w-1/2 h-auto lg:h-full px-12 lg:px-24 py-24 gap-8">
+          <Contentful document={heroTitle} />
+          <Contentful document={heroText} />
         </div>
       </div>
     </main>
--- src/components/Contentful/Contentful.tsx
+++ src/components/Contentful/Contentful.tsx
@@ -6,49 +6,49 @@ type TextProps = {children: React.ReactNode; className?: string};
 
 export const H1 = ({children, className = ''}: TextProps) => (
   <h1
-    className={`font-h text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-white ${className}`}>
+    className={`whitespace-break-spaces font-h text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-white ${className}`}>
     {children}
   </h1>
 );
 
 export const H2 = ({children, className = ''}: TextProps) => (
   <h2
-    className={`font-h text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-white ${className}`}>
+    className={`whitespace-break-spaces font-h text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-white ${className}`}>
     {children}
   </h2>
 );
 
 export const H3 = ({children, className = ''}: TextProps) => (
   <h3
-    className={`font-h text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl text-white ${className}`}>
+    className={`whitespace-break-spaces font-h text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl text-white ${className}`}>
     {children}
   </h3>
 );
 
 export const H4 = ({children, className = ''}: TextProps) => (
   <h4
-    className={`font-h text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl text-white ${className}`}>
+    className={`whitespace-break-spaces font-h text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl text-white ${className}`}>
     {children}
   </h4>
 );
 
 export const H5 = ({children, className = ''}: TextProps) => (
   <h5
-    className={`font-h text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl text-white ${className}`}>
+    className={`whitespace-break-spaces font-h text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl text-white ${className}`}>
     {children}
   </h5>
 );
 
 export const H6 = ({children, className = ''}: TextProps) => (
   <h6
-    className={`font-h text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl text-white ${className}`}>
+    className={`whitespace-break-spaces font-h text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl text-white ${className}`}>
     {children}
   </h6>
 );
 
 export const P = ({children, className = ''}: TextProps) => (
   <p
-    className={`font-p text-xs sm:text-sm md:text-sm lg:text-base xl:text-base text-white ${className}`}>
+    className={`whitespace-break-spaces font-p text-xs sm:text-sm md:text-sm lg:text-base xl:text-base text-white ${className}`}>
     {children}
   </p>
 );
