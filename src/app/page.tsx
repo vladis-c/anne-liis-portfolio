5a78231c1700f906edebaa9009a18d9c8f56a2c9
Vladislav Cherkasheninov <vladislav.cherkasheninov@taiste.com>
2024-08-03 11:58:56 +0300

    removed scroll animation temporary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,28 +15,26 @@ export default async function Home() {
 
   return (
     <main className="flex flex-col items-center justify-between">
-      <Scroll className="w-full">
-        {/* Navigation content */}
-        <Nav {...navigation} />
-        {/* Hero content */}
-        <Hero {...hero} />
-        {/* Sections menu */}
-        <Sections sections={sections} />
+      {/* Navigation content */}
+      <Nav {...navigation} />
+      {/* Hero content */}
+      <Hero {...hero} />
+      {/* Sections menu */}
+      <Sections sections={sections} />
+      <section
+        id="contacts-footer"
+        className="flex w-full h-auto lg:h-1080 bg-anne-indigo-medium">
         <section
           id="contacts-footer"
-          className="flex w-full h-auto lg:h-1080 bg-anne-indigo-medium">
-          <section
-            id="contacts-footer"
-            className="flex flex-col lg:flex-row justify-start lg:justify-between items-center w-full h-auto lg:h-1/2 bg-anne-indigo-light p-24">
-            <></>
-          </section>
-          <section
-            id="contacts-footer"
-            className="flex flex-col lg:flex-row justify-start lg:justify-between items-center w-full h-auto lg:h-1/2 p-24">
-            <></>
-          </section>
+          className="flex flex-col lg:flex-row justify-start lg:justify-between items-center w-full h-auto lg:h-1/2 bg-anne-indigo-light p-24">
+          <></>
         </section>
-      </Scroll>
+        <section
+          id="contacts-footer"
+          className="flex flex-col lg:flex-row justify-start lg:justify-between items-center w-full h-auto lg:h-1/2 p-24">
+          <></>
+        </section>
+      </section>
     </main>
   );
 }
