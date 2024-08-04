44840f5d1e6832d5cd6a255bdc549a0cd2dcf7ad
Vladislav Cherkasheninov <vladislav.cherkasheninov@taiste.com>
2024-08-04 10:25:57 +0300

    changed target env in script

--- scripts/run.sh
+++ scripts/run.sh
@@ -23,4 +23,4 @@ echo "Extracted file path: $file_path"
 
 # Execute the second command with auto-confirmation
 echo "Executing apply command with file path: $file_path"
-echo "Y" | contentful-merge apply --cma-token "$CMA_TOKEN" --space "$SPACE_ID" --environment "staging" --file "$file_path"
+echo "Y" | contentful-merge apply --cma-token "$CMA_TOKEN" --space "$SPACE_ID" --environment "$TARGET_BRANCH" --file "$file_path"
